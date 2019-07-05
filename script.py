80056901207466f0a6a266bcb91b3ee4e9008d15
hvandusen <henry@dusendusen.com>
2019-07-05 13:04:25 -0700

    okay something happening

--- script.py
+++ script.py
@@ -11,7 +11,7 @@ if(len(sys.argv)>1):
 response = google_images_download.googleimagesdownload()   #class instantiation
 for i in range(0,1):
     arguments = {"keywords":word,"limit":10,"offset":1,"print_urls":True,"no_directory":True,"no_numbering":True,
-    "extract_metadata": True, "no_download": True,"size": "medium"}   #creating list of arguments
+    "extract_metadata": True, "no_download": True,"size": "medium", "sa": True}   #creating list of arguments
     paths = response.download(arguments)   #passing the arguments to the function
-    # print(word)   #printing absolute paths of the downloaded images
+    print(paths)   #printing absolute paths of the downloaded images
     time.sleep(1)
--- server.js
+++ server.js
@@ -5,22 +5,70 @@ const port = process.env.PORT || 3002;// === "production" ? 80 : 3002;
 const fs = require("fs")
 const stream = require("./Stream.js");
 const spawn = require("child_process").spawn;
-const pythonProcess = spawn("python",["script.py"])
+// const pythonProcess = spawn("python",["script.py"])
+let wordLinks = [];
 let  words = "";
-pythonProcess.stdout.on('data', data => {
-  words += data
+let pythonProcess;
+let totalWordListSize = 0;
+function getUrlsFromWord(word){
+  return new Promise((resolve,reject) =>  {
+    pythonProcess = spawn("sh",["-c",'python script.py '+word+' | grep -o "http.*$"'])
+    pythonProcess.stdout.on('data', data => {
+      resolve(data.toString().split("https").join("https"))
+      console.log("got  data for "+word)
+    });
+    pythonProcess.stderr.on('data', data => {
+      reject(data.toString())
+    });
+  });
+}
+var addedWords = [];
+
+const isValidUrl = (e) => {
+  console.log("herei s e",e)
+  return e.indexOf("https://www.facebook.com")<0
+  && e !== "";
+}
+
+let getWords = (count=10) => stream(count).then(newWords => {
+  wordLinks  = [];
+  let googleSearches = newWords.map((newWord,i) => {
+    wordLinks.push({word: newWord})
+    return getUrlsFromWord(newWord)
+  })
+  Promise.all(googleSearches).then( wordSearches => {
+    console.log("RESOLVED!!!")
+    for(i in wordSearches){
+      let urls = wordSearches[i];
+      wordLinks[i].urls = urls.split("\n").filter( e => e.length>0);
+    };
+    // wordLinks = addedWords;
+    // addedWords = [];
+  })
 });
-const getWords =  () => stream().then(e => words += e.join(", "));
 
-getWords() ;
+getWords();
 
-app.get("*", (req, res) => {
-  res.send(words)
+// setInterval(getWords,100000)
+
+app.use('/', express.static("dist"));
+
+app.get("/json", (req, res) => {
+  res.send(wordLinks)
   getWords();
+  console.log(wordLinks)
 })
 
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
+
+// app.get("/", (req, res) => {
+  // res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+// })
+
 // app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "build", "index.html"));
+//
 // });
 
 app.listen(port, () => console.log(`listening on port ${port}!`))
