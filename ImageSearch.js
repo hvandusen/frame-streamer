5be88ea84aebe5abb63cbb84e3de7e56e657473d
Henry Van Dusen <henry@dusendusen.com>
2020-05-13 15:48:31 -0400

    slowed down, and image fits to canvas

--- ImageSearch.js
+++ ImageSearch.js
@@ -2,12 +2,10 @@
 const GoogleImages = require('google-images');
 //const client = new GoogleImages(process.env.GOOGLE_CSE_ID,process.env.GOOGLE_API_KEY);
 const client = new GoogleImages(process.env.GOOGLE_CSE_ID,process.env.GOOGLE_API_KEY);
-
 function search(word){
     const opts = {
       start: Math.floor(Math.random()*100)
     }
     return client.search(word,opts)
 }
-
 module.exports = search
--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ updateCanvas()
 function getCanvasImgs(){
   let response = axios.get('/json').then(function(resp){
     let newImages = resp.data.images
+    console.log("got data",resp.data)
     interval = resp.data.interval
     images = images.concat(newImages)
     .filter((img,i,self) => {
@@ -48,50 +49,20 @@ function updateCanvas(){
 }
 
 
-function fitImageToCanvas(ctx, img, x, y, w, h, offsetX, offsetY) {
-
-    if (arguments.length === 2) {
-        x = y = 0;
-        w = ctx.canvas.width;
-        h = ctx.canvas.height;
+function fitImageToCanvas(ctx, img) {
+    var i = {
+      w : img.width,
+      h : img.height,
+      r: img.width/ img.height
+    }, c= {
+      w: canvas.width,
+      h: canvas.height,
+      r: canvas.width/ canvas.height
+    };
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if(i.r < c.r){ //Image taller than canvas?
+      ctx.drawImage(img, (c.w-(c.h*i.r))/2, 0, c.h*i.r, c.h);
+    } else {
+      ctx.drawImage(img, 0, (c.h-(c.w/i.r))/2, c.w, c.w/i.r);
     }
-
-    // default offset is center
-    offsetX = typeof offsetX === "number" ? offsetX : 0.5;
-    offsetY = typeof offsetY === "number" ? offsetY : 0.5;
-
-    // keep bounds [0.0, 1.0]
-    if (offsetX < 0) offsetX = 0;
-    if (offsetY < 0) offsetY = 0;
-    if (offsetX > 1) offsetX = 1;
-    if (offsetY > 1) offsetY = 1;
-
-    var iw = img.width,
-        ih = img.height,
-        r = Math.min(w / iw, h / ih),
-        nw = iw * r,   // new prop. width
-        nh = ih * r,   // new prop. height
-        cx, cy, cw, ch, ar = 1;
-
-    // decide which gap to fill
-    if (nw < w) ar = w / nw;
-    if (Math.abs(ar - 1) < 1e-14 && nh < h) ar = h / nh;  // updated
-    nw *= ar;
-    nh *= ar;
-
-    // calc source rectangle
-    cw = iw / (nw / w);
-    ch = ih / (nh / h);
-
-    cx = (iw - cw) * offsetX;
-    cy = (ih - ch) * offsetY;
-
-    // make sure source rectangle is valid
-    if (cx < 0) cx = 0;
-    if (cy < 0) cy = 0;
-    if (cw > iw) cw = iw;
-    if (ch > ih) ch = ih;
-
-    // fill image in dest. rectangle
-    ctx.drawImage(img, cx, cy, cw, ch,  x, y, w, h);
 }
